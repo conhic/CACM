097b3e4ca107b7051ab602852d6c71a50479263c
Connor James Hickman <conhic@inf.upv.es>
2017-02-08 17:34:51 +0100

    Accepted with time: 0.120

--- 120-Stacks_of_Flapjacks/Main.java
+++ 120-Stacks_of_Flapjacks/Main.java
@@ -1,6 +1,6 @@
-/**
+ /**
  * Created by Connor on 06/02/2017.
- * Accepted with time: 0.440s
+ * Accepted with time: 0.120
  */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -15,15 +15,16 @@ class Main{
 
         //TODO: Change array for linkedList
         while((line = in.readLine()) != null){
-            Scanner scanner = new Scanner(line);
             System.out.printf(line + "\n");
 
-            int[] pancakes = new int[line.split(" ").length];
+            String[] lineSplit = line.split(" ");
+            int[] pancakes = new int[lineSplit.length];
 
             //Populate pancake stack reversed to make computation logic easier
-            for(int i = pancakes.length - 1; i >= 0; i--){
-                int diameter = scanner.nextInt();
-                pancakes[i] = diameter;
+            for(int i = 0; i < pancakes.length; i++){
+                int diameter = Integer.parseInt(lineSplit[i]);
+                //Flip on insert
+                pancakes[pancakes.length - 1 - i] = diameter;
             }
 
             String output = order(pancakes);
@@ -37,28 +38,28 @@ class Main{
     public static String order(int[] stack){
         String output = ""; 	//Initialize with no moves
         int begin = 0; 			//Indicates current number of correct elements
-		int end = stack.length - 1;
+        int end = stack.length - 1;
 
         while(!isSorted(stack)){
             int flipIndex = maxInSubset(stack, begin);
 
             //If the maximum value is in the top position of the stack,
             //after flipping it will be in the correct position
-			if(flipIndex == end){
-				//Flip max from top into correct position
-				flip(stack, begin);
+            if(flipIndex == end){
+                //Flip max from top into correct position
+                flip(stack, begin);
                 output += (begin + 1) + " ";
                 begin++;
             } else if(flipIndex == begin){
                 begin++;
             } else {
-				//Flip max to top
+                //Flip max to top
                 flip(stack, flipIndex);
                 output += (flipIndex + 1) + " ";
-				
-				//Flip max from top into correct position
+
+                //Flip max from top into correct position
                 flip(stack, begin);
-				output += (begin + 1) + " ";
+                output += (begin + 1) + " ";
                 begin++;
             }
         }
