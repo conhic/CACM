ef212b28a8e76028743717cb17de51555cbaebb7
Connor James Hickman <conhic@inf.upv.es>
2017-02-08 15:58:16 +0100

    Accepted with time: 0.160

---
+++ 10041-Vitos_Family/Main.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by Connor on 08/02/2017.
+ * Accepted with time: 0.160
+ *
+ * Observations: BufferedReader > Scanner (Scanner is almost 0.5 seconds slower)
+ */
+class Main{
+
+    public static void main(String[]args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int cases = Integer.parseInt(in.readLine());
+        for(int i = 0; i < cases; i++){
+            //Scanner scanner = new Scanner(in.readLine());
+            String[] line = in.readLine().split(" ");
+
+            int numRelatives = Integer.parseInt(line[0]);
+            int[] relatives = new int[numRelatives];
+            for(int j = 0; j < numRelatives; j++){
+                relatives[j] = Integer.parseInt(line[j + 1]);
+            }
+
+            //order(relatives);     //CountingSort was removed. java.util sorting was 0.020s faster
+            Arrays.sort(relatives);
+            int mediana = relatives[numRelatives / 2];
+            int distance = 0;
+
+            for(int j : relatives){
+                distance += Math.abs(j - mediana);
+            }
+
+            System.out.printf("%d\n", distance);
+        }
+    }
+}
